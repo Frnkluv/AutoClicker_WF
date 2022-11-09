6c30595ade1a575fc2c7cb25201a20a0d7fa9cc5
Egor Medv <egormed043@gmail.com>
2022-11-09 22:48:35 +0300

    Add project files.

---
+++ AutoClicker_WF/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AutoClicker_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Start = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Update = new System.Windows.Forms.Timer(this.components);
+            this.Clicker = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Snow;
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Start.Location = new System.Drawing.Point(32, 104);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(312, 42);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Press to Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.trackBar1.Location = new System.Drawing.Point(32, 24);
+            this.trackBar1.Maximum = 240000;
+            this.trackBar1.Minimum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(312, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(167, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1000";
+            // 
+            // Update
+            // 
+            this.Update.Interval = 1;
+            this.Update.Tick += new System.EventHandler(this.Update_Tick);
+            // 
+            // Clicker
+            // 
+            this.Clicker.Tick += new System.EventHandler(this.Clicker_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CapsLock";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "1 sec";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(289, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "4 min";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(374, 174);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Update;
+        private System.Windows.Forms.Timer Clicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ AutoClicker_WF/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoClicker_WF
+{
+    public partial class Form1 : Form
+    {
+        // хук, который перехватывает нажатие клавиши
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+
+        const uint LeftDown = 0x0002;   // нажатие на лкм
+        const uint LeftUp = 0x0004;     // поднятие(отпускание) лкм
+
+        private bool PressButton { get; set; } = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Update.Start();
+            Clicker.Stop();
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        // Start
+        private void Start_Click(object sender, EventArgs e)
+        {
+            //Clicker.Start();
+
+            if (sender is Button startButton)
+            {
+                
+                if (PressButton)
+                {
+                    Clicker.Start();
+                    startButton.BackColor = Color.LightGreen;
+                    PressButton = false;
+                    Start.Text = "ON";
+                }
+                else
+                {
+                    Clicker.Stop();
+                    startButton.BackColor = Color.Snow;     //нет привязки на дефолтный цвет. Если изм в форме, то вручную менять тут.
+                    PressButton = true;
+                    Start.Text = "Press to start";
+                }
+            }
+        }
+
+        // Stop
+        //private void Stop_Click(object sender, EventArgs e)
+        //{
+        //    Clicker.Stop();
+        //}
+
+        private void Update_Tick(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+            Clicker.Interval = trackBar1.Value;
+        }
+
+        private void Clicker_Tick(object sender, EventArgs e)
+        {
+            // если убрать локер то я хер выключу, из-за мильона кликов
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                mouse_event(LeftDown, 0, 0, 0, (UIntPtr)0);
+                mouse_event(LeftUp, 0, 0, 0, (UIntPtr)0);
+            }
+            else return;
+        }
+    }
+}
